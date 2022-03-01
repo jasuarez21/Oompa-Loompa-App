de229e070737e4d36479dd102565ba4f201e2562
Josep Suarez <djose.anton@gmail.com>
2022-03-01 11:16:23 +0100

    Create header component

--- oompa-loompa-app/src/App.js
+++ oompa-loompa-app/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
+import { Header } from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <h1>
-     Oompa Loompa
-    </h1>
+    <Header />
   );
 }
 
---
+++ oompa-loompa-app/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+
+export const Header = () => {
+  return (
+      <header class="header-container">
+        <img src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png" alt="Oompa-Loompas Logo" class="header-container__logo"  />
+        <p class="header-container__title">Oompa Loompa's Crew</p>
+      </header>
+  )
+}
