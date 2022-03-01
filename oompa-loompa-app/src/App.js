7f5da9cccaf905468a0779d71dcdcc4a66ccb35d
Josep Suarez <djose.anton@gmail.com>
2022-03-01 12:51:49 +0100

    Fix error in routing

--- oompa-loompa-app/src/App.js
+++ oompa-loompa-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import {
-  Router, Routes, Route
+  BrowserRouter, Routes, Route
 } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { DetailOfOompa } from './components/DetailOfOompa/DetailOfOompa'
@@ -11,13 +11,13 @@ import configureStore from './redux/store';
 function App() {
   return (
     <Provider store={configureStore()}>
-      <Router>
+      <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/list" component={ListOfOompas} />
-          <Route path="/:id" component={DetailOfOompa} />
-        </Routes>
-      </Router>
+          <Route exact path="/" element={<ListOfOompas/>} />
+          <Route path="/:id" component={<DetailOfOompa/>} />
+        </Routes> 
+      </BrowserRouter>
    </Provider>
   );
 }
