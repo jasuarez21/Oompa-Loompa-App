f466b403beabbe6c1e2001000703e67d34e89b14
Josep Suarez <djose.anton@gmail.com>
2022-03-04 10:26:08 +0100

    Add .env to enviroment of app

--- oompa-loompa-app/src/redux/actions/actionCreators.js
+++ oompa-loompa-app/src/redux/actions/actionCreators.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 export function loadOompas(){
     return async (dispatch) => {
         try {
-          const { data } = await axios('https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas');
+          const { data } = await axios(process.env.REACT_APP_ENV_URL_OOMPAS);
           dispatch({
             type: actionTypes.LOAD_OOMPAS,
             oompas: data
           });
         } catch (error) {
           dispatch({
-            type: 'LOAD_OOMPAS_ERROR'
+            type: actionTypes.LOAD_OOMPAS_ERROR
           });
         }
       };
@@ -20,14 +20,14 @@ export function loadOompas(){
 export function loadOompasDetail(idOompa){
   return async (dispatch) => {
       try {
-        const { data } = await axios(`https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas/${idOompa}`);
+        const { data } = await axios(process.env.REACT_APP_ENV_URL_OOMPAS+idOompa);
         dispatch({
           type: actionTypes.LOAD_OOMPA_DETAIL,
           oompa: data
         });
       } catch (error) {
         dispatch({
-          type: 'LOAD_OOMPAS_ERROR'
+          type: actionTypes.LOAD_OOMPAS_ERROR
         });
       }
     };
